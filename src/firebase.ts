c72f786bc1cb41357c75b97632f8df6a4681184e
Chiangtale <hjh1231@naver.com>
2024-10-16 21:54:40 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,6 +4,13 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
+  apiKey: "AIzaSyBdxz_Ri5wK-df4wL2ZFNcAG5bwSI9b2-Y",
+  authDomain: "chiangtale.firebaseapp.com",
+  projectId: "chiangtale",
+  storageBucket: "chiangtale.appspot.com",
+  messagingSenderId: "570297251521",
+  appId: "1:570297251521:web:56acf621178e87f552c640",
+  measurementId: "G-QDWJTMP70D"
 
 };
 
